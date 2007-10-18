c8d2661d03f78be17cd0f61936a12879e00d78ce
obb770 <obb770@1c27dbda-a23c-0410-a298-7fa0d64c0f64>
2007-10-18 22:07:19 +0000

    changed the class chat to Chat

--- trunk/hat-compile.cmd
+++
@@ -1,2 +0,0 @@
-@set GWTDIR=D:\Software\Java\gwt-windows-1.4.60
-@java -cp "%~dp0src;%~dp0bin;%GWTDIR%\gwt-user.jar;%GWTDIR%\gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0www" %* com.benreuven.chat.chat
--- trunk/src/com/benreuven/chat/client/Chat.java
+++ trunk/src/com/benreuven/chat/client/Chat.java
@@ -7,7 +7,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class chat implements EntryPoint {
+public class Chat implements EntryPoint {
 
   /**
    * This is the entry point method.
